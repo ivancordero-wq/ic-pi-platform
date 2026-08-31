84d400f4e188079a651dc4c071bc24f7eeea62d3
ivancordero-wq <ivan.cordero@iccommerce.us>
2026-08-31 15:02:26 -0500

    Register template upload router in main.py

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ from app.routes.theta_gate import router as theta_gate_router
 from app.routes.sme_ranking import router as sme_ranking_router
 from app.routes.formula_generation import router as formula_generation_router
 from app.routes.data_template import router as data_template_router
+from app.routes.template_upload import router as template_upload_router
 from app.routes.kpi_weighting import router as kpi_weighting_router
 from app.routes.sme_kpi_ranking import router as sme_kpi_ranking_router
 from app.routes.tau_designation import router as tau_designation_router
@@ -94,6 +95,7 @@ app.include_router(theta_gate_router)
 app.include_router(sme_ranking_router)
 app.include_router(formula_generation_router)
 app.include_router(data_template_router)
+app.include_router(template_upload_router)
 app.include_router(kpi_weighting_router)
 app.include_router(sme_kpi_ranking_router)
 app.include_router(tau_designation_router)
